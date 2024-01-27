38da1b3c2e442b97ea108b457ea735cf7504fb5f
Karabo <karabondhlovu19@gmail.com>
2024-01-27 16:47:56 +0200

    added paris

--- src/index.js
+++ src/index.js
@@ -24,4 +24,4 @@ function handleSearchSubmit(event){
 }
 let searchFormElement=document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
-searchCity("");
+searchCity("Paris");
