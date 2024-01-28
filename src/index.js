cdbc4315c5020f2a12f2d82b7557f69a7202a732
Karabo <karabondhlovu19@gmail.com>
2024-01-28 15:27:30 +0200

    updated weather data

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function refreshWeather(response){
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
-    let date=newDate=formatDate(Date);
+    let date = new Date(response.data.time * 1000)
 
     cityElement.innerHTML=response.data.city;
       timeElement.innerHTML = formatDate(date);
@@ -15,7 +15,33 @@ function refreshWeather(response){
       windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     tempearatureElement.innerHTML=Math.round(temperature);
 
+
 }
+
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+
+
 function searchCity(city){
 let apikey = "b2a5adcct04b33178913oc335f405433";
 let apiurl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
