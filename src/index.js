653dd9eb531ae61f5dfc2a66b6e4e2c311d8ae3f
Karabo <karabondhlovu19@gmail.com>
2024-01-28 15:18:02 +0200

    added date and time format

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ function refreshWeather(response){
     let tempearatureElement=document.querySelector("#temperature");
     let temperature=response.data.temperature.current;
     let cityElement=document.querySelector("#city");
+    let descriptionElement=document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date=newDate=formatDate(Date);
 
     cityElement.innerHTML=response.data.city;
+      timeElement.innerHTML = formatDate(date);
+      descriptionElement.innerHTML = response.data.condition.description;
+      humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+      windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     tempearatureElement.innerHTML=Math.round(temperature);
 
 }
