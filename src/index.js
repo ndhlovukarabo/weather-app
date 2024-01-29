b276f5d8f5ea09e87a5000b81faa123be9500cd4
Karabo <karabondhlovu19@gmail.com>
2024-01-29 11:46:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ function refreshWeather(response){
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
-    let date = new Date(response.data.time * 1000)
-    let iconElement=document.querySelector("#icon")
+    let date = new Date(response.data.time * 1000);
+    let iconElement=document.querySelector("#icon");
 
 
 
@@ -22,8 +22,8 @@ function refreshWeather(response){
         ` <img
            src="${response.data.condition.icon_url}"
            class="weather-app-icon"
-         />`
-       ;
+         />`;
+       
 
 }
 
